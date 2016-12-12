d62c73808d82f5f16e79c5df1b85a8fa3751b4cc
PabloVG <pablovilchezg@hotmail.com>
2016-12-12 13:32:17 +0100

    Elimino archivos de SIM808

--- Documentacion/Tutoriales/SIM808/Mini GSM_GPRS + GPS Breakout - SIM808 - Wiki_files/load.php
+++
@@ -1,2 +0,0 @@
-mw.loader.state({"site":"ready"});
-/* cache key: seeedc6_wikidb2:resourceloader:filter:minify-js:7:bd6e88e91c5e8b9c8e1a906e6fa7f64d */<script type='text/javascript' src=''></script>
